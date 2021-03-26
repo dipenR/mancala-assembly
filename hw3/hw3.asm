0cdb8ff34f752bc5adc7f0a4703ad85c23d1168f
Dipen Rupani <dipen.rupani@stonybrook.edu>
2021-03-26 01:08:00 -0400

    Loaded all required files

---
+++ hw3/hw3.asm
@@ -0,0 +1,38 @@
+# Dipen Rupani
+# drupani
+# 112321338
+
+############################ DO NOT CREATE A .data SECTION ############################
+############################ DO NOT CREATE A .data SECTION ############################
+############################ DO NOT CREATE A .data SECTION ############################
+
+.text
+
+load_game:
+	jr $ra
+get_pocket:
+	jr $ra
+set_pocket:
+	jr $ra
+collect_stones:
+	jr $ra
+verify_move:
+	jr  $ra
+execute_move:
+	jr $ra
+steal:
+	jr $ra
+check_row:
+	jr $ra
+load_moves:
+	jr $ra
+play_game:
+	jr  $ra
+print_board:
+	jr $ra
+write_board:
+	jr $ra
+	
+############################ DO NOT CREATE A .data SECTION ############################
+############################ DO NOT CREATE A .data SECTION ############################
+############################ DO NOT CREATE A .data SECTION ############################
